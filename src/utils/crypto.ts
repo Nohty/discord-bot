8a464dff8a4694f8a7324dfaf408aceb7e2c1d63
daan hautekiet <daanhautekiet@gmail.com>
2022-10-19 20:03:34 +0200

    Created decrypt function

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,4 +1,4 @@
-import { CipherCCMTypes, createCipheriv, randomBytes } from "crypto";
+import { CipherCCMTypes, createCipheriv, createDecipheriv, randomBytes } from "crypto";
 
 /**
  * A class for encrypting and decrypting data.
@@ -41,5 +41,19 @@ export class Crypto {
     encrypted = Buffer.concat([encrypted, cipher.final()]);
 
     return iv.toString("hex") + ":" + encrypted.toString("hex");
+  }
+
+  public decrypt(text: string): string {
+    const textParts = text.split(":");
+    if (textParts.length !== 2) throw new Error("Invalid cipher text.");
+
+    const iv = Buffer.from(textParts[0], "hex");
+    const encryptedText = Buffer.from(textParts[1], "hex");
+
+    const decipher = createDecipheriv(this._algorithm, Buffer.from(this._key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted.toString();
   }
 }
