d5d3be60568f3b2edc87695cfcd3f84ffa57dfb7
daan hautekiet <daanhautekiet@gmail.com>
2022-10-19 15:51:13 +0200

    Added commands and events log

--- src/events/ready.event.ts
+++ src/events/ready.event.ts
@@ -1,4 +1,5 @@
 import { BaseEvent, DiscordClient } from "../structures";
+import { syncCommands } from "../utils";
 
 export default class ReadyEvent extends BaseEvent {
   constructor() {
@@ -8,5 +9,10 @@ export default class ReadyEvent extends BaseEvent {
   public async run(client: DiscordClient): Promise<any> {
     console.log(`Bot has logged in as ${client.user.username}#${client.user.discriminator}!`);
     client.editStatus("online", { name: "with TypeScript", type: 0 });
+
+    await syncCommands(client);
+
+    console.log(`\nBot has loaded ${client.commands.size} commands!`);
+    console.log(`Bot has loaded ${client.listeners.length} events!`);
   }
 }
